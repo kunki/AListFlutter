82c2895bdd85a8a65bc9820426e01478ead328db
kunki <kunki@users.noreply.github.com>
2025-06-23 20:13:25 +0800

    Update init_alist.sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/alist-org/alist.git"
-#TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
-TAG_NAME="beta"
+GIT_REPO="https://github.com/OpenListTeam/OpenList.git"
+TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
+#TAG_NAME="beta"
 
 echo "AList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/alist-org/alist.git ./src
+git clone --branch "$TAG_NAME" https://github.com/OpenListTeam/OpenList.git ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
